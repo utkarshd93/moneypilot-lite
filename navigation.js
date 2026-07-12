fe43411ef4c6d0201f4a79695556ceb3b6c19b7d
utkarshd93 <utkarshd93@gmail.com>
2026-07-12 16:08:41 +0530

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -152,6 +152,17 @@ document.getElementById("addTransactionSheet");
 
 const closeSheetButton =
 document.getElementById("closeSheetBtn");
+
+const transactionMiniBar =
+document.getElementById("transactionMiniBar");
+
+let sheetState="closed";
+
+let startY=0;
+
+let currentY=0;
+
+let dragging=false;
 function openBottomSheet(){
 
     if(typeof syncTransactionDate==="function"){
@@ -274,59 +285,3 @@ if(viewAllButton){
 ===================================================== */
 
 openPage("home");
-
-const transactionMiniBar =
-
-document.getElementById(
-
-"transactionMiniBar"
-
-);
-
-function minimizeTransactionSheet(){
-
-    if(!bottomSheet) return;
-
-    bottomSheet.classList.remove(
-
-        "show"
-
-    );
-
-    transactionMiniBar.classList.add(
-
-        "show"
-
-    );
-
-}
-
-function restoreTransactionSheet(){
-
-    if(!bottomSheet) return;
-
-    bottomSheet.classList.add(
-
-        "show"
-
-    );
-
-    transactionMiniBar.classList.remove(
-
-        "show"
-
-    );
-
-}
-
-if(transactionMiniBar){
-
-    transactionMiniBar.addEventListener(
-
-        "click",
-
-        restoreTransactionSheet
-
-    );
-
-}
