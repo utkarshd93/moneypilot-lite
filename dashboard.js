901e3badd586f1aed42fd4ea854ed80c7e49d7bc
utkarshd93 <utkarshd93@gmail.com>
2026-07-27 14:20:20 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -397,7 +397,7 @@ Available Balance
 
 <h2>
 
-₹${summary.remaining.toLocaleString("en-IN")}
+₹${summary.availableBalance.toLocaleString("en-IN")}
 
 </h2>
 
@@ -421,7 +421,7 @@ onclick="editBenefitBalance('${summary.category}')">
 
 <strong>
 
-₹${summary.loaded.toLocaleString("en-IN")}
+₹${summary.monthlyLoaded.toLocaleString("en-IN")}
 
 </strong>
 
@@ -433,7 +433,7 @@ onclick="editBenefitBalance('${summary.category}')">
 
 <strong>
 
-₹${summary.spent.toLocaleString("en-IN")}
+₹${summary.monthlySpent.toLocaleString("en-IN")}
 
 </strong>
 
